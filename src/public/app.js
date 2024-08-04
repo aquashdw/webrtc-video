523d230a986618a1271b77ac7672cb1b8dd74985
aquashdw <aquashdw@gmail.com>
2024-08-04 22:36:23 +0900

    flow to show chat room

--- src/public/app.js
+++ src/public/app.js
@@ -1,3 +1,34 @@
 const socket = io();
 
+// procedural divs
+const nicknameDiv = document.getElementById("nickname");
+const selectHostDiv = document.getElementById("select-host");
+const chatRoomDiv = document.getElementById("chat-room");
 
+// Set Nickname
+const nicknameForm = nicknameDiv.querySelector("form");
+nicknameForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input")
+  const value = input.value;
+  socket.emit("nickname", input.value, () => {
+    nicknameDiv.classList.add("d-none");
+    selectHostDiv.classList.remove("d-none");
+    document.getElementById("my-nickname").innerText = `${value} (Me)`
+  });
+  input.value = "";
+});
+
+// Become host (create room)
+document.getElementById("host-button").addEventListener("click", (event) => {
+  socket.emit("create_room", () => {
+    selectHostDiv.classList.add("d-none");
+    chatRoomDiv.classList.remove("d-none");
+    document.getElementById("select-host-button").parentElement.remove();
+  });
+});
+// Become guest (pass to next)
+document.getElementById("guest-button").addEventListener("click", (event) => {
+  selectHostDiv.classList.add("d-none");
+  chatRoomDiv.classList.remove("d-none");
+});
--- src/server.js
+++ src/server.js
@@ -14,7 +14,36 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app)
 const io = new Server(httpServer);
 
+// collect room info
+const countRoom = (roomName) => {
+  const rooms = io.sockets.adapter.rooms;
+  return rooms.get(roomName)?.size;
+};
 
+const getRoomList = () => {
+  const roomInfoList = [];
+  const {sids, rooms} = io.sockets.adapter;
+  rooms.forEach((_, room) => {
+    if (sids.get(room) === undefined) roomInfoList.push({
+      room, users: countRoom(room),
+    })
+  });
+  return roomInfoList;
+}
+
+io.on("connection", (socket) => {
+  socket.emit("rooms", getRoomList());
+  socket.on("nickname", (nickname, done) => {
+    socket.nickname = nickname;
+    done();
+  });
+
+  socket.on("create_room", (done) => {
+    socket.join(socket.nickname);
+    io.sockets.emit("rooms", getRoomList());
+    done();
+  });
+});
 
 const handleListen = () => console.log("Listening on http://127.0.0.1:3000")
 httpServer.listen(3000, handleListen);
